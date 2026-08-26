e5e2c1fff9daba3ad5c54b7be13ff1188ae33cef
Mark Yavorskyi <myavorskyi@google.com>
2026-08-26 15:04:02 +0000

    Restructure Lottie sample demo app

--- sample/src/main/java/com/google/android/horologist/lottie/LottieScreen.kt
+++ sample/src/main/java/com/google/android/horologist/lottie/LottieScreen.kt
@@ -23,14 +23,10 @@ import androidx.compose.remote.creation.compose.modifier.RemoteModifier
 import androidx.compose.remote.creation.compose.modifier.fillMaxSize as remoteFillMaxSize
 import androidx.compose.remote.player.compose.RemoteDocumentPlayer
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalWindowInfo
 import com.google.android.horologist.remotecompose.lottie.LottieAnimation
